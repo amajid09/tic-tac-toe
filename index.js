abcaa9f76d1812f8c9655809fa48ce80af03c056
amajid09 <amajid0981@gmail.com>
2023-12-30 14:10:36 +0200

    finally done with the project, made it more modular. using factory function in javascript.

--- index.js
+++ index.js
@@ -1,160 +1,217 @@
-let board = [
+
+
+const Player = (sign) => {
+    let _sign = sign;
+    const getSign = () => _sign;
+    const setSign = (sign) => _sign=sign;
+    return {getSign, setSign}
+}
+
+const GameBoard = (sign) => {
+    let _sign = sign;
+    let board = [
     [ 1, 2, 3 ],
     [ 4, 5, 6 ],
     [ 7, 8, 9 ],
-]
-let turn = "player 1"
-let taken = "x";
-function run(input){
-        if( !istaken( input ) ){
-            turn = turn == "player 1" ? "player 2" : "player 1" 
-            taken = taken == "x" ? "o" : "x"
-            updateBoard(input)
-            renderBoard(input, taken)
-         
-    }
-}
-function renderBoard(input, taken) {
-    let items = document.querySelectorAll(".item")
-    items.forEach(i => {
-        if (i.classList[1] == input){
-            i.textContent = taken
+ ];
+    const isWinning = () => right() || top() || middle() || center() || left() || bottom() || diagonal();
+    
+    const top = () =>{
+        let count = 0;
+        for(let i = 0;i < 3; i++) {
+            if(board[0][i] == _sign){
+                count++;
+            }
         }
-})
-}
-function updateBoard(input){
-    let number = parseInt(input);
-    let row = getRow(number)
-    let col = getCol(number, row) 
-    board[row][col] = taken
-    console.log("updated...")
-}
-function getCol(number, row){
-    return (number-1) - 3*row;
-}
-function getRow(number) {
-    return Math.trunc((number-1) / 3);
-}
-function istaken( input ) {
-    let number = parseInt(input)
-    let row = getRow(number) 
-    let col = getCol(number, row)
-    return board[row][col] == taken;
-}
-async function startGame(){
-    //listen for click events
-    let item = document.querySelectorAll(".item")
-    let input; 
-    item.forEach(i => {
-        i.addEventListener("click", function(e) {
-            input = i.classList[1]
-            if(isWinning()) {
-                document.querySelector("h1").textContent = taken.toUpperCase() + " WON!"
-                return;
-            } 
-            run(input)  
-            console.log(board)       
+        return count == 3;
+    };
+
+    const center =() => {
+        let count = 0;
+        for(let i = 0;i < 3; i++) {
+            if(board[1][i] == _sign) {
+                count ++;
+            }
+        }
+        return count == 3;
+    }
+
+    const bottom = () => {
+        let count = 0;
+        for(let i = 0;i< 3; i++) {
+            if(board[2][i] == _sign) {
+                count++;
+            }
+        }
+        return count == 3;
+    }
+    const right = () => {
+        let count = 0; 
+        for (let i = 0; i < 3; i++ ){
+            if(board[i][0] == _sign) {
+                count ++;
+            }
+        }
+        return count == 3;
+    }
+
+    const middle = () => {
+        let count = 0;
+        for (let i = 0; i < 3; i++ ){
+            if(board[i][1] == _sign) {
+                count++;
+            }
+        }
+        return count == 3;
+    }
+    const left = () => {
+        let count = 0;
+        for (let i = 0; i < 3; i++ ){
+            if(board[i][2] == _sign) {
+                count++;
+            }
+        }
+        return count == 3;
+    }
+    const diagonal = () => {
+        return ( board[0][0] == _sign && board[1][1] == _sign && board[2][2] == _sign ) ||
+        ( board[2][0] == _sign  && board[1][1] == _sign && board[0][2] == _sign );
+    }
+
+
+    const getBoard = () => board;
+    const setBoard =(i, j, value) => board[i][j] = value;
+    const resetBoard = () => {
+        for(let i = 0; i< 3; i++) {
+            for (let j = 0; j < 3; j++){
+                board[i][j] = (i*3) + j+1;
+            }
+        }
+        let items = document.querySelectorAll(".item");
+        items.forEach(i=> {
+            i.textContent = "";
         })
-    })
-}
-function isWinning(){
-    return right() || top() || middle() || center() || left() || bottom() || diagonal()
-}
-function top(){
-    let count = 0;
-    for(let i = 0;i < 3; i++) {
-        if(board[0][i] == taken){
-            count++;
-        }
+        document.querySelector("h1").textContent = "";
     }
-    return count == 3;
+    
+    return {getBoard, setBoard, resetBoard, isWinning };
 }
 
-function center(){
-    let count = 0;
-    for(let i = 0;i < 3; i++) {
-        if(board[1][i] == taken) {
-            count ++;
-        }
-    }
-    return count == 3;
-}
 
-function bottom() {
-    let count = 0;
-    for(let i = 0;i< 3; i++) {
-        if(board[2][i] == taken) {
-            count++;
-        }
+const Game = (player, input) => {
+    let _player = player;
+    let _input = input; 
+    let board = GameBoard(_player.getSign());
+    const setPlayer = () => _player.getSign() == "x" ? 
+    _player.setSign("o") : _player.setSign("x");
+;
+    const setInput = (input) => _input = input;
+    const  getCol = (number, row) => (number-1) - 3*row;
+    const getRow = (number) =>  Math.trunc((number-1) / 3);
+    const getPlayer = () => player; 
+    const updateBoard = () => {
+        let number = parseInt(_input);
+        let row = getRow(number);
+        let col = getCol(number, row);
+        console.log("row " + row + "\ncolumn " + col) 
+        board.setBoard(row, col, _player.getSign());
     }
-    return count == 3;
-}
-function right(){
-    let count = 0; 
-    for (let i = 0; i < 3; i++ ){
-        if(board[i][0] == taken) {
-            count ++;
-        }
+    
+    const renderBoard = () => {
+        let items = document.querySelectorAll(".item");
+        console.log("input " + _input)
+        items.forEach(i => {
+            if (i.classList[1] == _input){
+                i.textContent = _player.getSign() ;
+            }
+        })
     }
-    return count == 3;
-}
+    const isTaken = () => {
+        let number = parseInt(_input);
+        let row = getRow(number);
+        let col = getCol(number, row);
+        console.log(_input)
+        console.log(board.getBoard()[row][col])
+        return ["x", "o"].indexOf(board.getBoard()[row][col]) > -1;
+    }
+    const isWinning = () => board.isWinning()
+    const isDraw = () => {
+        let count = 0; 
+        for(let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if(["x", "o"].indexOf(board.getBoard()[i][j]) > -1){
+                    count++;
+                }
+            }
+        }
+        return count == 9;
+    };
 
-function middle() {
-    let count = 0;
-    for (let i = 0; i < 3; i++ ){
-        if(board[i][1] == taken) {
-            count++;
-        }
+    const resetBoard = () => board.resetBoard();
+    return {updateBoard,resetBoard, isDraw, renderBoard, setPlayer, setInput, isTaken, isWinning};
+}
+const Main = () => {
+    const player = Player("x");
+    const game =  Game(player, -1);
+    const run = () => {
+            game.updateBoard();
+            game.renderBoard();
+
     }
-    return count == 3;
-}
-function left(){
-    let count = 0;
-    for (let i = 0; i < 3; i++ ){
-        if(board[i][2] == taken) {
-            count++;
-        }
+    const startGame = () => {
+        let item = document.querySelectorAll(".item");
+        let input; 
+        item.forEach(i => {
+            i.addEventListener("click", function(e) {
+                input = i.classList[1]
+                game.setInput(input)
+                if(!game.isTaken()){
+                    run()
+                    game.setPlayer()
+                }
+                if(game.isWinning()) {
+                    let won = player.getSign() == "x" ? "o" : "x"; 
+                    document.querySelector("h1").textContent = won.toUpperCase() + " WON!"
+                    return;
+                }
+                if(game.isDraw()) {
+                    document.querySelector("h1").textContent = "DRAW!"
+                    return;
+                }
+            })
+        })
     }
-    return count == 3;
-}
-function diagonal() {
-    return ( board[0][0] == taken && board[1][1] == taken && board[2][2] == taken ) ||
-    ( board[2][0] == taken  && board[1][1] == taken && board[0][2] == taken )
-}
-function restartGame(){
-    for(let i = 0; i< 3; i++) {
-        for (let j = 0; j < 3; j++){
-            board[i][j] = (i*3) + j+1
-        }
+    const restartGame = () => {
+        document.querySelector("button").addEventListener("click", function(e){
+            game.resetBoard();
+            startGame()
+        })
     }
-    let items = document.querySelectorAll(".item");
-    items.forEach(i=> {
-        i.textContent = ""
-    })
-    document.querySelector("h1").textContent = ""
-}
-startGame()
-document.querySelector("button").addEventListener("click", function(e){
-    restartGame();
-    startGame()
-})
+    const removeBoarders = () => {
+        let items = document.querySelectorAll(".item")
+        items.forEach(i=> {
+            let lb = ["4","6"];
+            let bt = ["2", "8"]
+        if( lb.indexOf(i.classList[1]) > -1 ) {
+                i.style = "border-left:none;border-right:none"
+        } 
+        else if(bt.indexOf(i.classList[1]) > -1 ) {
+                i.style = "border-bottom:none;border-top:none"
+        }
+        else if(i.classList[1] != 5){
+                i.style = "border:none;"
+        }
+
+        })
+    }
+    return {removeBoarders,startGame, restartGame}
+};
 
 
-function removeBorders(){
-    let items = document.querySelectorAll(".item")
-    items.forEach(i=> {
-        let lb = ["4","6"];
-        let bt = ["2", "8"]
-       if( lb.indexOf(i.classList[1]) > -1 ) {
-            i.style = "border-left:none;border-right:none"
-       } 
-       else if(bt.indexOf(i.classList[1]) > -1 ) {
-            i.style = "border-bottom:none;border-top:none"
-       }
-       else if(i.classList[1] != 5){
-            i.style = "border:none;"
-       }
+let main = Main()
+main.removeBoarders()
+main.startGame()
+main.restartGame();
 
-    })
-}
-removeBorders()
+
+
